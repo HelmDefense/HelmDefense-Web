0b4761614bf20ca956793f8504349d1c15d7f883
Telio <teliobot@gmail.com>
2020-11-17 17:48:43 +0100

    beginning of api static page

---
+++ www/modules/mod_static/pages/api/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Documentation API</title>
+        <link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<main>
+			<h1 id="h1api">API</h1>
+
+			<section id="section1" class="defaultSec">
+				<article id="article1">
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+				</article>
+			</section>
+
+			<section id="section2" class="defaultSec">
+				<article id="article2">
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+				</article>
+			</section>
+
+			<section id="section3" class="defaultSec">
+				<article id="article3">
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+                    <p>
+                        Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam.
+                        Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor
+                        patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus
+                        hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus
+                        istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi
+                        meae et meum erga te parentemque tuum beneficium tueri debeo.
+                    </p>
+				</article>
+			</section>
+
+            <div id="link">
+                <a href="www.youtube.com">helmdefense.fr</a>
+            </div>
+		</main>
+	</body>
+</html>
+
