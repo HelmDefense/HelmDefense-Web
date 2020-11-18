efebfdca7262bfb25114e95884d3a0cced982636
mateo <mateolaville@hotmail.fr>
2020-11-18 09:34:38 +0100

    pre merge

--- www/modules/mod_static/pages/contact/index.php
+++ www/modules/mod_static/pages/contact/index.php
@@ -7,6 +7,7 @@
     <title>Contact</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="/data/css/form.css" rel="stylesheet">
+    <link href="/data/css/style.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
 
@@ -16,6 +17,13 @@
     <h1>Contact</h1>
 
     <form>
+
+        <div class="id">
+            <input id="id" name="id" type="text" placeholder="required" />
+
+        </div>
+
+    </form>
 
 </body>
 
