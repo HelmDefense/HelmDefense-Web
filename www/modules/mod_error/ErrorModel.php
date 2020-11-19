5b6799c6f4c12b7e6466c9020d920110f39f6e1f
indyteo <theo.mail@laposte.net>
2020-11-19 22:37:06 +0100

    Finished error module

---
+++ www/modules/mod_error/ErrorModel.php
@@ -0,0 +1,18 @@
+<?php
+include_once "modules/generic/Model.php";
+
+class ErrorModel extends Model {
+	/**
+	 * @param string|null $page
+	 * @return array
+	 */
+	public function searchFor($page) {
+		if (is_null($page))
+			return array();
+
+		$pages = array_diff(scandir("modules/mod_static/pages"), array(".", ".."));
+		return array_filter($pages, function ($p) use ($page) {
+			return stristr($page, $p) || stristr($p, $page);
+		});
+	}
+}
