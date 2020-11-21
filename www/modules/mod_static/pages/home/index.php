d185632bf5833a780cb88097ebb43f8901590e81
alexandrekd <Toulouse3177/>
2020-11-21 13:43:25 +0100

    Texte

--- www/modules/mod_static/pages/home/index.php
+++ www/modules/mod_static/pages/home/index.php
@@ -1,63 +1,45 @@
-<!doctype html>
-<html lang="fr">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css">
-
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="/data/css/style.css">
-	<link rel="stylesheet" href="style.css">
-
-	<title>Home - HelmDefense</title>
-</head>
-<body>
-
-
-	<div id="slider" class="carousel slide" data-ride="carousel">
-		<ol class="carousel-indicators">
-			<li data-target="#slider" data-slide-to="0" class="active"></li>
-			<li data-target="#slider" data-slide-to="1"></li>
-			<li data-target="#slider" data-slide-to="2"></li>
-		</ol>
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="image1">
+<div id="slider" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#slider" data-slide-to="0" class="active"></li>
+		<li data-target="#slider" data-slide-to="1"></li>
+		<li data-target="#slider" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-acceuil carousel-inner" role="listbox">
+		<div class="carousel-item active" style="background-image: url(https://via.placeholder.com/1000);">
+			<div class="">
+				<h2 class="col-12">bienvenue sur</h2>
+				<h1 class="col-12">Helm Denfense</h1>
 			</div>
-			<div class="item">
-				<img src="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg" alt="image2">
-			</div>
-			<div class="item">
-				<img src="https://lh3.googleusercontent.com/proxy/3-rjtgqNNc0dTYD743icV63Hi2DrjLaPwALxoQOxb_-5p09RqNVtH0o7WC9qCTk_H26oxhy7xufSOY62cb_3OIXa2Gb0iG4Iu2jjx2ZKuR1BrVyb8uncs29dzJkn6KYQJbJHsZX6wCVA3o6l5LIWQ-0wDanq8w389oc8Fc7Q1e54lQ" alt="image3">
-			</div>
-
 		</div>
-		<a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>>
-		</a>
-		<a class="carousel-control-next" href="#slider" role="button" data-slide="next">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>>
-		</a>
+		<div class="carousel-item" style="background-image: url(https://via.placeholder.com/1500);">
+			<h2>consultez</h2>
+			<h1>le Wiki</h1>
+		</div>
+		<div class="carousel-item" style="background-image: url(https://via.placeholder.com/500);">
+			<h2>échangez sur</h2>
+			<h1>le Forum</h1>
+		</div>
 
 	</div>
+	<a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">next</span>
+	</a>
+</div>
+<div class="container">
+	<div class="presentation row">
+		<div class="col-12 col-lg-6">
+			<img src="https://via.placeholder.com/400 " class="rounded" alt="...">
+		</div>
+		<div class="col-12 col-lg-6">
+			<h2 class="mt-lg-0">Présentation du jeu</h2>
 
-	<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-		<path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 8.5a.5.5 0 0 1 0-1h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5z"/>
-	</svg>
-
-</body>
-</html>
-
-
-<!--
-Red = 8E010F
-noir = 444444
-
-
-
--->
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fugiat maiores perferendis reprehenderit? Aliquam amet, asperiores consequuntur culpa debitis nam non omnis possimus quam quibusdam suscipit tempora vitae, voluptate voluptatum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium autem dolorem eius esse excepturi explicabo fuga, harum inventore laborum maiores, molestiae nemo odit, officiis perspiciatis quisquam repellendus repudiandae temporibus!</p>
+		</div>
+	</div>
+</div>
