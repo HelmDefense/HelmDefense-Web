d327cac8399ca0e3d9ad2d0e89b386e7225294eb
alexandrekd <Toulouse3177/>
2020-11-21 14:46:00 +0100

    Carousel + text

--- www/modules/mod_static/pages/home/index.php
+++ www/modules/mod_static/pages/home/index.php
@@ -6,18 +6,31 @@
 	</ol>
 	<div class="carousel-acceuil carousel-inner" role="listbox">
 		<div class="carousel-item active" style="background-image: url(https://via.placeholder.com/1000);">
-			<div class="">
-				<h2 class="col-12">bienvenue sur</h2>
-				<h1 class="col-12">Helm Denfense</h1>
+			<div class="d-flex align-items-center h-100">
+				<div class="mx-auto box text-center">
+					<h2 class="col-12">bienvenue sur</h2>
+					<p class="col-12">Helm Denfense</p>
+					<a href="#" class="btn carousel-btn ">Téléchargement</a>
+				</div>
 			</div>
 		</div>
 		<div class="carousel-item" style="background-image: url(https://via.placeholder.com/1500);">
-			<h2>consultez</h2>
-			<h1>le Wiki</h1>
+			<div class="d-flex align-items-center h-100">
+				<div class="mx-auto box text-center">
+					<h2 class="col-12">consultez</h2>
+					<p class="col-12">le Wiki</p>
+					<a href="#" class="btn carousel-btn ">Wiki</a>
+				</div>
+			</div>
 		</div>
 		<div class="carousel-item" style="background-image: url(https://via.placeholder.com/500);">
-			<h2>échangez sur</h2>
-			<h1>le Forum</h1>
+			<div class="d-flex align-items-center h-100">
+				<div class="mx-auto box text-center">
+					<h2 class="col-12">échangez sur</h2>
+					<p class="col-12">le Forum</p>
+					<a href="#" class="btn carousel-btn ">Forum</a>
+				</div>
+			</div>
 		</div>
 
 	</div>
@@ -30,7 +43,7 @@
 		<span class="sr-only">next</span>
 	</a>
 </div>
-<div class="container">
+<div class="container w-100">
 	<div class="presentation row">
 		<div class="col-12 col-lg-6">
 			<img src="https://via.placeholder.com/400 " class="rounded" alt="...">
@@ -38,8 +51,8 @@
 		<div class="col-12 col-lg-6">
 			<h2 class="mt-lg-0">Présentation du jeu</h2>
 
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fugiat maiores perferendis reprehenderit? Aliquam amet, asperiores consequuntur culpa debitis nam non omnis possimus quam quibusdam suscipit tempora vitae, voluptate voluptatum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium autem dolorem eius esse excepturi explicabo fuga, harum inventore laborum maiores, molestiae nemo odit, officiis perspiciatis quisquam repellendus repudiandae temporibus!</p>
+			<p>L'univers du jeu est inspiré de la Terre du Milieu (univers crée par J.R.R Tolkien et utilisé pour ses livres Seigneur des Anneaux, le Hobbit,le Silmarilliion, etc...).</p>
+			<p>Nous focalisons notre jeu sur le passage de la bataille du gouffre de Helm. Pour rappel, la scène se déroule à la fin du second tome du Seigneur des Anneaux (Les Deux Tours). Les forces du Rohan ont été contraintes à se terrer dans le gouffre de Helm pour échapper aux Orcs et Uruk-Haï envoyés par Saroumane, sous l’emprise de Sauron. Aux côtés des humains se rangent les Elfes de Haldir (venu de la Lothlórien), ainsi que le nain Gimli. En face, il y avait des Orcs, des Uruk-Haï et des Trolls. À la fin de la bataille, les Rohirrim (cavaliers du Rohan) attaquent les troupes du mal et ils remportent la victoire.</p>
 		</div>
 	</div>
 </div>
