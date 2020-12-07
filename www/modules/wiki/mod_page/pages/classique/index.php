71cdde497c70141818a24daafd872db3ac2b5b42
alexandrekd <Toulouse3177/>
2020-12-07 10:40:35 +0100

    Update page

---
+++ www/modules/wiki/mod_page/pages/classique/index.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+	<div class="col-5">
+		<img src="https://via.placeholder.com/500">
+		<h2>Titre</h2>
+	</div>
+	<div class="col-5">
+		<div>
+<!--			Appel MarkDownText-->
+		</div>
+	</div>
+	<div class="col-2">
+		
+	</div>
+</div>
