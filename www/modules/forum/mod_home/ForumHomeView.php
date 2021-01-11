43d054d59107a031de50ca2f5867667dc8c492ab
indyteo <theo.mail@laposte.net>
2021-01-11 21:02:05 +0100

    Created forum home module

---
+++ www/modules/forum/mod_home/ForumHomeView.php
@@ -0,0 +1,65 @@
+<?php
+namespace Module;
+
+use Utils;
+
+class ForumHomeView extends View {
+	public function homePage($homeText, $limit) { ?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12 col-xl-9">
+						<h2 class="forum-title">Forum Helm Defense</h2>
+						<div class="forum-body">
+							<p>Bienvenue sur le Forum de Helm Defense.</p>
+							<div class="text-justify">
+								<?= Utils::markdown($homeText); ?>
+							</div>
+							<h3 class="section-title">Discussions</h3>
+							<div class="forum-post-list">
+    							<?= Utils::renderComponent("forumpostlist", "talk", $limit) ?>
+							</div>
+							<div class="forum-link-container">
+								<a class="forum-link" href="/forum/talk">Plus de sujets dans la catégorie Discussions</a>
+							</div>
+							<h3 class="section-title">Avis sur les entités</h3>
+							<div class="forum-post-list">
+    							<?= Utils::renderComponent("forumpostlist", "rate", $limit) ?>
+							</div>
+							<div class="forum-link-container">
+								<a class="forum-link" href="/forum/rate">Plus de sujets dans la catégorie Avis sur les entités</a>
+							</div>
+							<h3 class="section-title">Stratégies</h3>
+							<div class="forum-post-list">
+    							<?= Utils::renderComponent("forumpostlist", "strat", $limit) ?>
+							</div>
+							<div class="forum-link-container">
+								<a class="forum-link" href="/forum/strat">Plus de sujets dans la catégorie Stratégies</a>
+							</div>
+						</div>
+					</div>
+					<div class="forum-sidebar-container col-12 col-xl-3">
+						<?= Utils::renderComponent("forumsidebar"); ?>
+					</div>
+				</div>
+			</div>
+	<?php }
+
+	public function postList($type) { ?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12 col-xl-9">
+						<h2 class="forum-title">Forum Helm Defense</h2>
+						<div class="forum-body">
+							<h3 class="section-title mt-0"><?= $type == "talk" ? "Discussions" : $type == "rate" ? "Avis sur les entités" : "Stratégies" ?></h3>
+							<div class="forum-post-list">
+								<?= Utils::renderComponent("forumpostlist", $type, 15, 1, true) ?>
+							</div>
+						</div>
+					</div>
+					<div class="forum-sidebar-container col-12 col-xl-3">
+						<?= Utils::renderComponent("forumsidebar"); ?>
+					</div>
+				</div>
+			</div>
+	<?php }
+}
