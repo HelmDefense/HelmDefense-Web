5248bd43cc5148a71870ce78bab1b0cd86e40c02
indyteo <theo.mail@laposte.net>
2021-01-11 20:59:48 +0100

    Fixed forum post list responsive

--- www/components/com_forumpostlist/ForumPostListView.php
+++ www/components/com_forumpostlist/ForumPostListView.php
@@ -7,56 +7,61 @@ class ForumPostListView extends View {
 	private static $i = 0;
 
 	public function displayTalkList($talks) {
-		$this->displayTable(array("Sujet", "Auteur", "Nombre de messages", "Date de création", "Dernière activité"), function() use ($talks) {
+		$this->displayTable(array(), function() use ($talks) {
 			foreach ($talks as $talk) { ?>
 				<tr>
 					<td><a href="/forum/talk/<?= $talk->id ?>"><?= htmlspecialchars($talk->title) ?></a></td>
 					<td><a href="/user/profile/<?= $talk->author->id ?>"><?= htmlspecialchars($talk->author->name) ?></a></td>
-					<td><?= htmlspecialchars($talk->message_count) ?></td>
-					<td><?= Utils::formatDate($talk->created_at) ?></td>
-					<td><?= Utils::formatDate($talk->last_activity) ?></td>
+					<td class="d-none d-md-table-cell"><?= htmlspecialchars($talk->message_count) ?></td>
+					<td class="d-none d-lg-table-cell"><?= Utils::formatDate($talk->created_at) ?></td>
+					<td class="d-none d-md-table-cell"><?= Utils::formatDate($talk->last_activity) ?></td>
 				</tr>
 			<?php }
 		});
 	}
 
 	public function displayRateList($rates) {
-		$this->displayTable(array("Sujet", "Entité", "Note", "Auteur", "Nombre de messages", "Date de création", "Dernière activité"), function() use ($rates) {
+		$this->displayTable(array("Entité", "Note"), function() use ($rates) {
 			foreach ($rates as $rate) { ?>
 				<tr>
 					<td><a href="/forum/rate/<?= $rate->id ?>"><?= htmlspecialchars($rate->title) ?></a></td>
-					<td><a href="/wiki/page/entity/<?= $rate->entity->id ?>"><?= htmlspecialchars($rate->entity->name) ?></a></td>
-					<td><?= htmlspecialchars($rate->rate) ?></td>
+					<td class="d-none d-sm-table-cell"><a href="/wiki/page/entity/<?= $rate->entity->id ?>"><?= htmlspecialchars($rate->entity->name) ?></a></td>
+					<td class="d-none d-sm-table-cell"><?= htmlspecialchars($rate->rate) ?></td>
 					<td><a href="/user/profile/<?= $rate->author->id ?>"><?= htmlspecialchars($rate->author->name) ?></a></td>
-					<td><?= htmlspecialchars($rate->message_count) ?></td>
-					<td><?= Utils::formatDate($rate->created_at) ?></td>
-					<td><?= Utils::formatDate($rate->last_activity) ?></td>
+					<td class="d-none d-md-table-cell"><?= htmlspecialchars($rate->message_count) ?></td>
+					<td class="d-none d-lg-table-cell"><?= Utils::formatDate($rate->created_at) ?></td>
+					<td class="d-none d-md-table-cell"><?= Utils::formatDate($rate->last_activity) ?></td>
 				</tr>
 			<?php }
 		});
 	}
 
 	public function displayStratList($strats) {
-		$this->displayTable(array("Sujet", "Niveau", "Héros", "Auteur", "Nombre de messages", "Date de création", "Dernière activité"), function() use ($strats) {
+		$this->displayTable(array("Niveau", "Héros"), function() use ($strats) {
 			foreach ($strats as $strat) { ?>
 				<tr>
 					<td><a href="/forum/strat/<?= $strat->id ?>"><?= htmlspecialchars($strat->title) ?></a></td>
-					<td><a href="/wiki/page/level/<?= $strat->level->id ?>"><?= htmlspecialchars($strat->level->name) ?></a></td>
-					<td><a href="/wiki/page/entity/<?= $strat->hero->id ?>"><?= htmlspecialchars($strat->hero->name) ?></a></td>
+					<td class="d-none d-sm-table-cell"><a href="/wiki/page/level/<?= $strat->level->id ?>"><?= htmlspecialchars($strat->level->name) ?></a></td>
+					<td class="d-none d-sm-table-cell"><a href="/wiki/page/entity/<?= $strat->hero->id ?>"><?= htmlspecialchars($strat->hero->name) ?></a></td>
 					<td><a href="/user/profile/<?= $strat->author->id ?>"><?= htmlspecialchars($strat->author->name) ?></a></td>
-					<td><?= htmlspecialchars($strat->message_count) ?></td>
-					<td><?= Utils::formatDate($strat->created_at) ?></td>
-					<td><?= Utils::formatDate($strat->last_activity) ?></td>
+					<td class="d-none d-md-table-cell"><?= htmlspecialchars($strat->message_count) ?></td>
+					<td class="d-none d-lg-table-cell"><?= Utils::formatDate($strat->created_at) ?></td>
+					<td class="d-none d-md-table-cell"><?= Utils::formatDate($strat->last_activity) ?></td>
 				</tr>
 			<?php }
 		});
 	}
 
 	private function displayTable($columns, $contentGenerator) { ?>
-		<table class="custom-table post-list" data-id="<?= ++self::$i ?>">
+		<table class="custom-table post-list w-100" data-id="<?= ++self::$i ?>">
 			<thead>
 				<tr>
-					<?php foreach ($columns as $column) echo "<th>$column</th>"; ?>
+					<th>Sujet</th>
+					<?php foreach ($columns as $column) echo "<th class='d-none d-sm-table-cell'>$column</th>"; ?>
+					<th>Auteur</th>
+					<th class="d-none d-md-table-cell">Mes&shy;sages</th>
+					<th class="d-none d-lg-table-cell">Date de création</th>
+					<th class="d-none d-md-table-cell">Dernière activité</th>
 				</tr>
 			</thead>
 			<tbody>
