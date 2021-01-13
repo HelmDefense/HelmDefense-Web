c6d7ce1534e00730eae83be8a8ab10dd6179c7d9
alexandrekd <alexandre.khefifderain@gmail.com>
2021-01-13 17:41:23 +0100

    disconnect

---
+++ www/modules/user/mod_signin/UserSigninModule.php
@@ -0,0 +1,18 @@
+<?php
+namespace Module;
+
+use Utils;
+
+include_once "modules/generic/Module.php";
+include_once "UserSigninController.php";
+
+class UserSigninModule extends Module {
+	public function __construct(){
+		parent::__construct(new UserSigninController());
+	}
+
+	protected function execute()
+	{
+		// TODO: Implement execute() method.
+	}
+}
